41da4e736ae3aa9ced8e9ca171c1bdc86d5825a8
ZarX4K <131523053+ZarX4K@users.noreply.github.com>
2024-11-22 10:58:36 +0100

    Delete src/main directory

--- src/main/java/AgeOfWar/Logic/DamageReductionMode.java
+++
@@ -1,9 +0,0 @@
-package AgeOfWar.Logic;
-
-public enum DamageReductionMode {
-    NORMAL,              // No armor reduction
-    ARMOR_ACTIVATED,     // Armor reduction activated
-    CRITICAL_ACTIVATED;  // Critical hit activated
-
-    // You can add more modes here as needed in the future
-}
